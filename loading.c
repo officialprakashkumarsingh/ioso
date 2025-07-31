27a55863f8574a0661d5ee3850386b308782a29a
officialprakashkumarsingh <officialprakashkrsingh@gmail.com>
2025-07-31 14:35:50 +0530

    Create loading.c

---
+++ loading.c
@@ -0,0 +1,3 @@
+// This file is intentionally left blank as part of the transition to a 3D Earth visualization project
+// The new project uses HTML, CSS, and JavaScript instead of C
+// Please see the new files: index.html, styles.css, and app.js
